24bd0ef95ff96ee49c7de14aa796f28dc33d45f1
Daniel Eriksson <gustaf.daniel.eriksson@gmail.com>
2016-02-09 00:08:05 +0100

    Adding correct directories and files

---
+++ src/java/se/abc/conspectus/parser/CSVParser.java
@@ -0,0 +1,44 @@
+package se.abc.conspectus.parser;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+public final class CSVParser implements DefinitionsParser {
+
+	public CSVParser() {
+	}
+
+	@Override
+	public Map<String, Set<String>> parse(final Path definitions) throws IOException {
+		try (Stream<String> stream = Files.lines(definitions)) {
+			/*
+			 * HashMap gives slightly faster lookups than TreeMap in this
+			 * application. Approximately 5 milliseconds versus 8 milliseconds
+			 * on my computer using a dictionary containing 30000 records.
+			 */
+			final Map<String, Set<String>> map = new HashMap<>();
+
+			stream.forEach(s -> {
+				final String[] values = s.split(",");
+				if (values.length > 1)
+					map.put(values[0],
+							new TreeSet<String>(
+									/*
+									 * Using Arrays.copyOfRange(values, 1,
+									 * values.length) is slightly faster, but
+									 * perhaps less obvious than subList.
+									 */
+									Arrays.<String> asList(values).subList(1, values.length)));
+			});
+
+			return map;
+		}
+	}
+}
