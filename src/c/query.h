17754cc9d53d12f984cc74d53faee56ec5662cc7
Daniel Eriksson <gustaf.daniel.eriksson@gmail.com>
2016-02-09 00:05:00 +0100

    Refreshing after changes to .gitignore

--- src/c/query.h
+++
@@ -1,6 +0,0 @@
-#ifndef QUERY_H_  
-#define QUERY_H_
-
-void error(char *s);
-
-#endif // QUERY_H_
--- src/sh/query.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Looks up synonyms by querying a Conspectus UDP server.
-#
-# Usage:
-# query.sh <search term>
-#
-SEARCH_TERM="$1"
-#
-query localhost 5005 "$SEARCH_TERM"
